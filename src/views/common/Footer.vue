362e06a5df16a11ed7c31c53c1ecf3ebdf59a645
zyz <43314748+zhenglix@users.noreply.github.com>
2020-08-05 16:27:25 +0800

    Update Footer.vue

--- src/views/common/Footer.vue
+++ src/views/common/Footer.vue
@@ -6,8 +6,8 @@
           <div class="tags has-addons">
             <a
               class="tag is-dark"
-              href="https://github.com/Aicirou/goindex-theme-acrou"
-            >goindex-theme-acrou</a>
+              href="https://wvw.lbdcj.top/"
+            >支援者</a>
             <span class="tag is-light">
               MIT &nbsp;
               <span class="icon">
